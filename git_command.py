065e5b7981f000c7597154df3910e2074ddc2599
Wojciech M <wojtekm@myprograme.com>
2022-06-18 10:24:14 +0200

    add form

--- git_command.py
+++ git_command.py
@@ -1,4 +1,16 @@
 import os
+def dowland_witch_git():
+    os.system("git fetch")
+    os.system("git pull")
+    save()
 
-os.system("git fetch")
-os.system("git pull")
+def save():
+    file = open("text.txt")
+    tab = file.readlines()
+    last = tab[0].replace("\n", "")
+    last = int(last)
+    with open("text.txt", "w") as file:
+        file.write(f"{last + 1}")
+
+if __name__ == '__main__':
+    dowland_witch_git()
